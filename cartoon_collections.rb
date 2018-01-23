07b210e9128ca4cd5cc9c2f7b549c0526169313b
K Logic <kellyayers411@gmail.com>
2018-01-23 14:21:56 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,8 +19,8 @@ end
 def find_the_cheese(strings)
   
   strings.each do |item|
-    
-    if (item == "cheddar")# || item == "gouda" || item == "camembert")
+    puts item
+    if (item == "cheddar") || item == "gouda" || item == "camembert")
       return item
     else
       return nil
